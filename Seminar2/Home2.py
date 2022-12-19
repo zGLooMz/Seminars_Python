3424871e48c7d7cac2ec991e795d06c8ff685ce3
Alexandr Zybailo <zero-pushkin@mail.ru>
2022-12-19 12:39:36 +0300

    Add Home2

---
+++ Seminar2/Home2.py
@@ -0,0 +1,29 @@
+# Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+# *Пример:*
+
+# - пусть N = 4, тогда [ 1, 2, 6, 24 ] (1, 1*2, 1*2*3, 1*2*3*4)
+
+def InputNumbers(text):
+    flag = False
+    while not flag:
+        try:
+            number = int(input(f"{text}"))
+            flag = True
+        except ValueError:
+            print("Нужно ввести целое число")
+    return number
+
+def recursion(n):
+    if n == 1:
+        return 1
+    else:
+        return n * recursion(n - 1)
+
+num = InputNumbers("Введите число: ")
+
+recursion_list = []
+for j in range(1, num + 1):
+    list.append(recursion(j))
+
+print(f"При N = {num}:  {recursion_list}")
