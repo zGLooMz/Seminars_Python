b66e7227e975a39b0e402f8e4ae035c523dc6c2e
Alexandr Zybailo <zero-pushkin@mail.ru>
2023-01-18 15:48:58 +0300

    Add Home4

---
+++ Seminar5/Home4.py
@@ -0,0 +1,47 @@
+# Реализуйте RLE алгоритм: реализуйте модуль сжатия и восстановления данных.
+
+with open('H:\Python\Seminars_Python\Seminar5\RLEdecoded.txt', 'r') as data:
+    decoded_text = data.read()
+
+
+def encode_rle(text):
+
+    str_code = ''
+    prev_char = ''
+    count = 1
+    if not text: return ''
+    for char in text:
+        if char != prev_char:
+            if prev_char:
+                str_code += str(count) + prev_char
+            count = 1
+            prev_char = char
+        else:
+            count += 1
+    else:
+        str_code += str(count) + prev_char
+
+    return str_code
+            
+str_code = encode_rle(decoded_text)
+print(f'Исходный текст ->', decoded_text)
+print(f'После сжатия   ->', str_code)
+
+with open('H:\Python\Seminars_Python\Seminar5\RLEencoded.txt', 'r') as data:
+    encoded_text = data.read()
+
+def decoding_rle(text:str):
+    count = ''
+    str_decode = ''
+    for char in text:
+        if char.isdigit():
+            count += char 
+        else:
+            str_decode += char * int(count)
+            count = ''
+    return str_decode
+
+str_decode = decoding_rle(encoded_text)
+print(f'Исходный текст      ->', encoded_text)
+print(f'После востановления ->',str_decode)
+
